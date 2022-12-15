e5697d152a23b2ef22124fcee4a45ecdf0e2e564
Matheus Rodrigues 🎸 <matheusr@microsoft.com>
2022-12-15 11:52:15 -0800

    move code base to typescript + webpack + auto reload dev

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Game } from "./components/game";
+
+const ctx = document.querySelector("canvas").getContext("2d");
+
+let game: Game;
+function startGame() {
+    game = new Game(ctx);
+    game.startGame();
+}
+
+function stopGame() {
+    if (game) {
+        game.stopGame();
+    }
+}
+
+function init() {
+    document.getElementById("startGameButton").addEventListener("click", startGame);
+    document.getElementById("stopGameButton").addEventListener("click", stopGame);
+}
+
+init();
