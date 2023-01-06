ba7e1f4ab9e80c564da539d850c939f140a08ca2
Rodrigues <matheus.affonso.cad@gmail.com>
2023-01-06 13:23:57 -0300

    more time upgrades

--- src/components/upgrade-manager/time-driven-upgrades.ts
+++ src/components/upgrade-manager/time-driven-upgrades.ts
@@ -9,9 +9,52 @@ export const TimeDrivenUpgrades = [
   {
     value: 1000 * 15,
     cb: (upgrade: UpgradeManager) => updateNewEnemyFrequency(upgrade.game, 1)
-  }
+  },
+  {
+    value: 1000 * 20,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.15)
+  },
+  {
+    value: 1000 * 25,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.15)
+  },
+  {
+    value: 1000 * 30,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.15)
+  },
+  {
+    value: 1000 * 35,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.15)
+  },
+  {
+    value: 1000 * 40,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.5)
+  },
+  {
+    value: 1000 * 45,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.5)
+  },
+  {
+    value: 1000 * 50,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.5)
+  },
+  {
+    value: 1000 * 55,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.5)
+  },
+  {
+    value: 1000 * 60,
+    cb: (upgrade: UpgradeManager) => increaseNewEnemyFrequency(upgrade.game, 0.1)
+  },
 ]
 
 function updateNewEnemyFrequency(game: Game, freq: number) {
   game.newObjectFrequency = freq;
+  console.log(`newObjectFrequency: ${game.newObjectFrequency}`);
 }
+
+function increaseNewEnemyFrequency(game: Game, perc: number) {
+  game.newObjectFrequency *= (1 + perc);
+  console.log(`newObjectFrequency: ${game.newObjectFrequency}`);
+}
+
